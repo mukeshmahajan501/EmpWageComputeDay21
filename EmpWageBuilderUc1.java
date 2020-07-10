0e07a5cbb57247ae1f3d774403231e3f2735d380
mukesh <mukeshmahajan501@gmail.com>
2020-07-10 18:14:54 +0530

    added employee present or absent code

---
+++ EmpWageBuilderUc1.java
@@ -0,0 +1,19 @@
+class EmpWageBuilderUc1
+{
+
+public static void main(String args[])
+{
+double no=Math.floor(Math.random()*10%2);
+System.out.println("no: "+no);
+	if (no==1)
+	{
+		System.out.println("employee is present");
+		System.out.println(no);
+	}
+	else
+	{
+		System.out.println("employee is absent");
+	}
+
+}
+}
